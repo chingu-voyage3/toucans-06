207ae894579e4ddbd1ef44d2d224204ad5a27fcb
Camper <freecodecamp@curious.work>
2017-12-15 23:23:02 -0500

    feat(header): add JavaScript and CSS for fixed-on-scroll

--- resources/js/main.js
+++ resources/js/main.js
@@ -28,9 +28,27 @@
  */
 
 window.onload = function() {
+    // select random number between 1 and 3, add innerHTML to video
+    // element with video file name
+
     let randomVideo = `video${Math.floor(Math.random() * 3) + 1}.mp4`;
     let videoElement = document.getElementsByTagName("video");
+
     videoElement[0].innerHTML = `
             <source src="./resources/videos/${randomVideo}" type="video/mp4">
             Videos of people doing different tasks in the nonprofit organizations we support.`
+};
+
+
+window.onscroll = function() {
+    // Detect when window is scrolled down more than 50 pixels, then add
+    // header-scrolled class and remove class when window is scrolled back to top.
+
+    let primaryHeaderElement = document.getElementsByClassName("primary-header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        primaryHeaderElement[0].classList.add("header-scrolled");
+    } else {
+        primaryHeaderElement[0].classList.remove("header-scrolled");
+    }
 };
